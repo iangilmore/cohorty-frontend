5f0a130aa777e7aeed7be136198f29d51b464226
Alexa Clark <lex.gwc@gmail.com>
2024-05-17 16:49:50 -0400

    Add react router dom and component imports to main and app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme.js';
@@ -14,7 +15,9 @@ import '@fontsource/roboto/700.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </ThemeProvider>
   </React.StrictMode>,
 )
--- src/pages/Course/Course.jsx
+++ src/pages/Course/Course.jsx
@@ -1,40 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import { getCourses } from '../../services/courses.js';
-import CourseItem from '../../components/CourseItem/CourseItem.jsx';
+import React from 'react'
 
 const Course = () => {
-  const [courses, setCourses] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchCourses = async () => {
-      try {
-        const data = await getCourses();
-        setCourses(data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchCourses();
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
-
   return (
-    <div className="course-page">
-      {courses.map((course) => (
-        <CourseItem key={course.id} course={course} />
-      ))}
-    </div>
-  );
-};
+    <div>Course</div>
+  )
+}
 
-export default Course;
-
-
-
+export default Course
