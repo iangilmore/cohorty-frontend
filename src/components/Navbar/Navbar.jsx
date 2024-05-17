02bbc1ac8610bd20eaa95e7c282e25e3fd27a368
Josh <jpgiant@yahoo.com>
2024-05-17 17:01:58 -0400

    Log out button add and showing hello button on screen

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,8 @@
+import './index.css'
+import Button from '@mui/material/Button';
+
+export default function LogOut() {
+
+  return <Button variant="outlined">Logout</Button>; 
+
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme.js';
+import CssBaseline from '@mui/material/CssBaseline';
 
 // Import Roboto font
 import '@fontsource/roboto/300.css';
@@ -14,6 +15,7 @@ import '@fontsource/roboto/700.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
