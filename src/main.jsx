01d6d66dcacaf5edd5ccb04fc44ce18f21d22b06
Alexa Clark <lex.gwc@gmail.com>
2024-05-17 14:03:03 -0400

    Add theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme.js';
 
 // Import Roboto font
 import '@fontsource/roboto/300.css';
@@ -11,6 +13,8 @@ import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
