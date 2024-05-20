7db5ee1604ff7fd497823e3f5127ed75e2a2436c
Josh <jpgiant@yahoo.com>
2024-05-20 10:24:42 -0400

    Assignment component fix

--- src/pages/Student/Student.jsx
+++ src/pages/Student/Student.jsx
@@ -1,26 +1,9 @@
-import { useState } from 'react';
-import AssignmentStatusGrid from "../../components/AssignmentStatusGrid/AssignmentStatusGrid";
-import Attendance from "../../components/Attendance/Attendance";
-import { Tabs, Tab, Box, Typography } from '@mui/material';
+import React from "react";
 
-export default function Student() {
-  const [selectedTab, setSelectedTab] = useState('assignments');
-
-  const handleChange = (event, newValue) => {
-    setSelectedTab(newValue);
-  };
-
+const Student = () => {
   return (
-    <Box sx={{ width: '100%' }}>
-      <Typography variant="h4" sx={{ my: 2, textAlign: 'center' }}>
-        Ian Gilmore
-      </Typography>
-      <Tabs value={selectedTab} onChange={handleChange} centered>
-        <Tab label="Assignments" value="assignments" />
-        <Tab label="Attendance" value="attendance" />
-      </Tabs>
-      {selectedTab === 'assignments' && <AssignmentStatusGrid />}
-      {selectedTab === 'attendance' && <Attendance />}
-    </Box>
-  );
+    <div>Student</div>
+  )
 }
+
+export default Student
