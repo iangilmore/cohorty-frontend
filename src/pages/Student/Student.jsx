397df199f6cba1d6ae472a68e170225eed9cdf5c
Josh <jpgiant@yahoo.com>
2024-05-20 10:31:47 -0400

    Student.jsx update

--- src/pages/Student/Student.jsx
+++ src/pages/Student/Student.jsx
@@ -1,9 +1,27 @@
-import React from "react";
 
-const Student = () => {
+import { useState } from 'react';
+import AssignmentStatusGrid from "../../components/AssignmentStatusGrid/AssignmentStatusGrid";
+import Attendance from "../../components/Attendance/Attendance";
+import { Tabs, Tab, Box, Typography } from '@mui/material';
+
+export default function Student() {
+  const [selectedTab, setSelectedTab] = useState('assignments');
+
+  const handleChange = (event, newValue) => {
+    setSelectedTab(newValue);
+  };
+
   return (
-    <div>Student</div>
-  )
+    <Box sx={{ width: '100%' }}>
+      <Typography variant="h4" sx={{ my: 2, textAlign: 'center' }}>
+        Ian Gilmore
+      </Typography>
+      <Tabs value={selectedTab} onChange={handleChange} centered>
+        <Tab label="Assignments" value="assignments" />
+        <Tab label="Attendance" value="attendance" />
+      </Tabs>
+      {selectedTab === 'assignments' && <AssignmentStatusGrid />}
+      {selectedTab === 'attendance' && <Attendance />}
+    </Box>
+  );
 }
-
-export default Student
