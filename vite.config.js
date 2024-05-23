ca75f38c3bfe6462fd3dd4d4ffcc790dcf44b506
Ian Gilmore <ian.gilmore@me.com>
2024-05-23 19:20:12 -0400

    minification test

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   // Enable minification (removes comments and whitespace)
-  //   minify: 'terser',
-  //   // Configure Terser options
-  //   terserOptions: {
-  //     compress: {
-  //       // Remove console logs
-  //       drop_console: true,
-  //     },
-  //   },
-  // },
+  build: {
+    // Enable minification (removes comments and whitespace)
+    minify: 'terser',
+    // Configure Terser options
+    terserOptions: {
+      compress: {
+        // Remove console logs
+        drop_console: true,
+      },
+    },
+  },
 })
