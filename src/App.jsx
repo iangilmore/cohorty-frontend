c2cec65eeb00a2e9dae55f49ff097047ae49a7f7
Josh <jpgiant@yahoo.com>
2024-05-23 17:32:44 -0400

    Update so that when user clicks on course Students is no longer highlighted and the correct page Assignemnts gets highlighted

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes, useLocation } from 'react-router-dom';
-import { useState } from 'react';
-import  { useEffect, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { AuthContext } from './context/AuthContextComponent.jsx';
 
 // Components
@@ -9,27 +8,32 @@ import Course from './pages/Course/Course.jsx';
 import Courses from './pages/Courses/Courses.jsx';
 import Login from './pages/Login/Login.jsx';
 import Student from './pages/Student/Student.jsx';
-import Navbar from './components/Navbar/Navbar.jsx'
-
+import Navbar from './components/Navbar/Navbar.jsx';
 
 // context
 function App() {
-  const { isUserLoggedIn } = useContext(AuthContext)
+  const { isUserLoggedIn } = useContext(AuthContext);
 
   const [activeTab, setActiveTab] = useState('assignments');
   const [courseName, setCourseName] = useState('');
   const [courseId, setCourseId] = useState('');
   const location = useLocation();
 
-  // Extract course name from the route state, if available
-  // const courseName = location.state?.courseName || ''; // Default course name
   // Determine if the current route is the login page
   const isLoginPage = location.pathname === '/';
+
+  useEffect(() => {
+    const pathSegments = location.pathname.split('/');
+    if (pathSegments.includes('students')) {
+      setActiveTab('students');
+    } else if (pathSegments.includes('assignments')) {
+      setActiveTab('assignments');
+    }
+  }, [location.pathname]);
 
   const handleTabChange = (tab) => {
     setActiveTab(tab);
   };
-
 
   return (
     <>
@@ -44,9 +48,8 @@ function App() {
             <Route path="/courses" element={<Courses setCourseId={setCourseId} setCourseName={setCourseName}/>} />
             <Route path="courses/:courseId/students" element={<Course activeTab="students" />} />
             <Route path="courses/:courseId/assignments" element={<Course activeTab="assignments" />} />
-             {/* <Route path="/:courseId" element={<Course activeTab={activeTab} />} /> */}
-            <Route path="/:courseId/students/:studentId" element={<Student />} />
-            <Route path="/:courseId/assignments/:assignmentId" element={<Assignment />} />
+            <Route path="courses/:courseId/students/:studentId" element={<Student />} />
+            <Route path="courses/:courseId/assignments/:assignmentId" element={<Assignment />} />
           </>
         }
       </Routes>
